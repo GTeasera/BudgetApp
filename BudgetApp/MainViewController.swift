84de7c371e31f1e7619df1381d7810e7a5f196bb
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-11-10 17:44:24 +0200

    🎉Application realise

---
+++ BudgetApp/MainViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MainViewController.swift
+//  BudgetApp
+//
+//  Created by Mykyta Tishkin on 10.11.2022.
+//
+
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    var tempBudget: Double?
+    var tempDays: Int?
+    var tempType: String?
+    
+    @IBOutlet weak var budgetF: UILabel!
+    @IBOutlet weak var daysF: UILabel!
+    @IBOutlet weak var perDayF: UILabel!
+    @IBOutlet weak var typeF: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        budgetF.text = String(tempBudget!)
+        daysF.text = String(tempDays!)
+        perDayF.text = String(tempBudget! / Double(tempDays!))
+        typeF.text = tempType
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func backToSettings(_ sender: Any) {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "settingsViewController") as! ViewController
+        controller.modalPresentationStyle = .fullScreen
+        present(controller, animated: true, completion: nil)
+    }
+}
---
+++ BudgetApp/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  BudgetApp
+//
+//  Created by Mykyta Tishkin on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var days: Int?
+    var type: String?
+    
+    @IBOutlet weak var budgetSize: UITextField!
+    @IBOutlet weak var daysBudget: UITextField!
+    @IBOutlet weak var segmentControll: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        budgetSize.keyboardType = .numbersAndPunctuation
+        daysBudget.keyboardType = .numberPad
+    }
+    
+    
+    @IBAction func typeOfBudget(_ sender: Any) {
+        if ((sender as AnyObject).selectedSegmentIndex == 0) {
+            type = "Economy"
+        }
+        else if ((sender as AnyObject).selectedSegmentIndex == 1) {
+            type = "InBudget"
+        }
+        else if ((sender as AnyObject).selectedSegmentIndex == 2) {
+            type = "Discounts"
+        }
+    }
+    
+    @IBAction func finishingSetup(_ sender: Any) {
+        days = Int(daysBudget.text!)
+        
+        let controller = storyboard?.instantiateViewController(withIdentifier: "actionStoryboard") as! MainViewController
+        var s = budgetSize.text
+        let modifier = s?.replace(",", with: ".")
+        
+        controller.tempBudget = Double(s!)
+        controller.tempDays = days
+        controller.tempType = type
+        
+        controller.modalPresentationStyle = .fullScreen
+        present(controller, animated: true, completion: nil)
+    }
+    
+
+}
+
